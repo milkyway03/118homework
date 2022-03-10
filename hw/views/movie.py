47dafd7d97a3b7f3162c4a841c45ac9e7d7de0fd
milkyway03 <95718076+milkyway03@users.noreply.github.com>
2022-03-10 16:12:55 +0300

    filters added

--- hw/views/movie.py
+++ hw/views/movie.py
@@ -1,8 +1,7 @@
-from warnings import filters
 
 from flask import request
 from flask_restx import Resource, Namespace
-from hw.dao.model.movie import MovieSchema
+from hw.dao.model.movie import MovieSchema, Movie
 from implemented import movie_service
 
 movie_ns = Namespace('movies')
@@ -11,10 +10,25 @@ movie_ns = Namespace('movies')
 @movie_ns.route("/")
 class MoviesView(Resource):
     def get(self):
-        genre = request.args.get("genre_id")
-        all_movies = movie_service.get_all(filters)
-        res = MovieSchema(many=True).dump(all_movies)
-        return res, 200
+        res = MovieSchema(many=True)
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        year = request.args.get('year')
+        if director_id and genre_id and year:
+            movies = Movie.query.filter_by(director_id=director_id, genre_id=genre_id).all()
+        elif director_id:
+            movies = Movie.query.filter_by(director_id=director_id).all()
+        elif genre_id:
+            movies = Movie.query.filter_by(genre_id=genre_id).all()
+        elif year:
+            movies = Movie.query.filter_by(year=year).all()
+        else:
+            movies = Movie.query.all()
+        if movies:
+            return res.dump(movies), 208
+        else:
+            return 404
+
 
     def post(self):
         req_json = request.json
@@ -22,7 +36,7 @@ class MoviesView(Resource):
         return "", 201, {"location": f"/movies/{movie.id}"}
 
 
-@movie_ns.route("f<int:bid>")
+@movie_ns.route("/<int:bid>")
 class MovieView(Resource):
     def get(self, bid):
         b = movie_service.get_one(bid)
@@ -30,7 +44,7 @@ class MovieView(Resource):
         return sm_d, 200
 
     def put(self, bid):
-        req_json= request.json
+        req_json = request.json
         if "id" not in req_json:
             req_json["id"] = bid
         movie_service.update(req_json)
